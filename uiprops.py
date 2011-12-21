4544e6777601f75ab7461aae9f9cdd21087eea3f
Carlos Balderas <carlos.balderas@gmail.com>
2011-12-21 13:36:49 -0600

    add foundation framework  v2.1.4

---
+++ uiprops.py
@@ -0,0 +1,7 @@
+# copyright 2011 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# contact http://www.logilab.fr -- mailto:contact@logilab.fr
+
+"""
+"""
+STYLESHEETS.append(data('stylesheets/foundation.css'))
+JAVASCRIPTS.append(data('javascripts/foundation.js'))
