0b74057f1459100538f7fa373ca3e4b30876bed5
Katia Saurfelt <katia.saurfelt@logilab.fr>
2012-06-28 18:16:13 +0200

    [forms] adapt the inline render

--- views/orbui_forms.py
+++ views/orbui_forms.py
@@ -14,14 +14,79 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
+from logilab.mtconverter import xml_escape
 
-from cubicweb.web.views.formrenderers import FormRenderer, EntityFormRenderer
-from cubicweb.web.views.autoform import AutomaticEntityForm
+from cubicweb.utils import json_dumps
+
+from cubicweb.web import formwidgets
+from cubicweb.web.views import formrenderers
+from cubicweb.web.views.autoform import AutomaticEntityForm, GenericRelationsField
 from cubicweb.web.views.forms import FieldsForm
 
 FieldsForm.needs_css = ()
+FieldsForm.cssclass = 'form-horizontal'
 
-class FormRendererOrbui(FormRenderer):
+class GenericRelationsWidgetOrbui(formwidgets.FieldWidget):
+    """override the GenericRelation default widget.
+       CubicWeb builds a table, in orbui, we build a simple list of divs :
+    """
+
+    def render(self, form, field, renderer):
+        stream = []
+        w = stream.append
+        req = form._cw
+        _ = req._
+        __ = _
+        eid = form.edited_entity.eid
+        for rschema, role, related in field.relations_table(form):
+            # already linked entities
+            if related:
+                label = rschema.display_name(req, role, context=form.edited_entity.__regid__)
+                w(u'<div class="control-group">')
+                w(u'<span class="form-label">%s</span>' % label)
+                w(u'<div class="controls">')
+                w(u'<ul>')
+                for viewparams in related:
+                    w(u'<li>%s <span id="span%s" class="%s">%s</span></li>'
+                      % (viewparams[1], viewparams[0], viewparams[2], viewparams[3]))
+                if not form.force_display and form.maxrelitems < len(related):
+                    link = (u'<span>'
+                            '[<a href="javascript: window.location.href+=\'&amp;__force_display=1\'">%s</a>]'
+                            '</span>' % _('view all'))
+                    w(u'<li>%s</li>' % link)
+                w(u'</ul>')
+                w(u'</div>')
+                w(u'</div>')
+        pendings = list(field.restore_pending_inserts(form))
+        if pendings:
+            for row in pendings:
+                # soon to be linked to entities
+                w(u'<div id="tr%s">' % row[1])
+                w(u'<span class="label label-warning">%s</span>' % row[3])
+                w(u'<a class="handle" title="%s" href="%s">[x]</a>&nbsp;' %
+                  (_('cancel this insert'), row[2]))
+                w(u'<a id="a%s" class="editionPending" href="%s">%s</a>'
+                  % (row[1], row[4], xml_escape(row[5])))
+                w(u'</div>')
+        w(u'<div class="row-fluid">')
+        w(u'<div id="relationSelectorRow_%s" class="span6">' % eid)
+        w(u'<select id="relationSelector_%s" tabindex="%s" '
+          'onchange="javascript:showMatchingSelect(this.options[this.selectedIndex].value,%s);">'
+          % (eid, req.next_tabindex(), xml_escape(json_dumps(eid))))
+        w(u'<option value="">%s</option>' % _('select a relation'))
+        for i18nrtype, rschema, role in field.relations:
+            # more entities to link to
+            w(u'<option value="%s_%s">%s</option>' % (rschema, role, i18nrtype))
+        w(u'</select>')
+        w(u'</div>')
+        w(u'<div id="unrelatedDivs_%s"></div>' % eid)
+        w(u'</div>')
+        return '\n'.join(stream)
+
+GenericRelationsField.widget = GenericRelationsWidgetOrbui
+GenericRelationsField.control_field = False
+
+class FormRendererOrbui(formrenderers.FormRenderer):
     """form renderer class
     """
     button_bar_class = u'form-actions'
@@ -29,7 +94,7 @@ class FormRendererOrbui(FormRenderer):
     def render_content(self, w, form, values):
         if self.display_progress_div:
             w(u'<div id="progress">%s</div>' % self._cw._('validating...'))
-        w(u'<fieldset class="form-horizontal">')
+        w(u'<fieldset>')
         self.render_fields(w, form, values)
         self.render_buttons(w, form)
         w(u'</fieldset>')
@@ -96,30 +161,28 @@ class FormRendererOrbui(FormRenderer):
             except KeyError:
                 self.warning('no such fieldset: %s (%s)', fieldset, form)
                 continue
-            w(u'<fieldset class="form-horizontal %s">\n' % (fieldset or
-                                                            u'default'))
+            w(u'<fieldset class="%s">' % (fieldset or u'default'))
             if fieldset:
                 w(u'<legend>%s</legend>' % self._cw._(fieldset))
             for field in fields:
-                w(u'<div class="control-group %s_%s_row">' % (field.name,
-                                                              field.role))
-                if self.display_label and field.label is not None:
-                    w(u'%s' %
-                      self.render_label(form, field))
-                w(u'<div')
-                if field.label is None:
-                    w(u' colspan="2"')
                 error = form.field_error(field)
-                if error:
-                    w(u' class="error"')
-                w(u' class="controls">')
+                control = not hasattr(field, 'control_field') or field.control_field
+                w(u'<div class="control-group %s-%s_row %s">' % (field.name,
+                                                                 field.role,
+                                                                 'error' if error else ''))
+                if self.display_label and field.label is not None:
+                    w(u'%s' % self.render_label(form, field))
+                if control:
+                    w(u'<div class="controls">')
+                else:
+                    w(u'<div class="controls no-margin">')
                 w(field.render(form, self))
-                w(u'')
                 if error:
                     self.render_error(w, error)
                 if self.display_help:
                     w(self.render_help(form, field))
-                w(u'</div></div>')
+                w(u'</div>')
+                w(u'</div>')
             w(u'</fieldset>')
         if byfieldset:
             self.warning('unused fieldsets: %s', ', '.join(byfieldset))
@@ -135,7 +198,7 @@ class FormRendererOrbui(FormRenderer):
         w(u'</div>')
 
 
-class EntityFormRendererOrbui(EntityFormRenderer):
+class EntityFormRendererOrbui(FormRendererOrbui, formrenderers.EntityFormRenderer):
     """This is the 'default' renderer for entity's form.
 
     You can still use form_renderer_id = 'base' if you want base FormRenderer
@@ -150,7 +213,7 @@ class EntityFormRendererOrbui(EntityFormRenderer):
             attrs_fs_label = (u'<legend>%s</legend>' %
                               self._cw._(self.main_form_title))
         open_form = u'%s%s' % (attrs_fs_label,
-                               super(EntityFormRenderer,
+                               super(formrenderers.EntityFormRenderer,
                                      self).open_form(form, values))
         return open_form
 
@@ -160,7 +223,7 @@ class EntityFormRendererOrbui(EntityFormRenderer):
         more than
         and <form>
         """
-        return super(EntityFormRenderer, self).close_form(form, values) + ''
+        return super(formrenderers.EntityFormRenderer, self).close_form(form, values) + ''
 
     def render_buttons(self, w, form):
         """let the form buttons be inside a div
@@ -169,51 +232,17 @@ class EntityFormRendererOrbui(EntityFormRenderer):
             w(u'<div class="form-actions"> %s %s %s </div>' %
               tuple(button.render(form) for button in form.form_buttons))
         else:
-            super(EntityFormRenderer, self).render_buttons(w, form)
+            super(formrenderers.EntityFormRenderer, self).render_buttons(w, form)
 
-    def _render_fields(self, fields, w, form):
-        """render form fields
-        """
-        byfieldset = {}
-        for field in fields:
-            byfieldset.setdefault(field.fieldset, []).append(field)
-        if form.fieldsets_in_order:
-            fieldsets = form.fieldsets_in_order
-        else:
-            fieldsets = byfieldset.keys()
-        for fieldset in fieldsets:
-            try:
-                fields = byfieldset.pop(fieldset)
-            except KeyError:
-                self.warning('no such fieldset: %s (%s)', fieldset, form)
-                continue
-            w(u'<fieldset class="form-horizontal %s">' % (fieldset or
-                                                          u'default'))
-            if fieldset:
-                w(u'<legend>%s</legend>' % self._cw._(fieldset))
-            for field in fields:
-                w(u'<div class="control-group %s_%s_row">' % (field.name,
-                                                              field.role))
-                if self.display_label and field.label is not None:
-                    w(u'%s' %
-                      self.render_label(form, field))
-                w(u'<div')
-                if field.label is None:
-                    w(u' colspan="2"')
-                error = form.field_error(field)
-                if error:
-                    w(u' class="error"')
-                w(u' class="controls">')
-                w(field.render(form, self))
-                w(u'')
-                if error:
-                    self.render_error(w, error)
-                if self.display_help:
-                    w(self.render_help(form, field))
-                w(u'</div></div>')
-            w(u'</fieldset>')
-        if byfieldset:
-            self.warning('unused fieldsets: %s', ', '.join(byfieldset))
+
+class EntityInlinedFormRendererOrbui(EntityFormRendererOrbui, formrenderers.EntityInlinedFormRenderer):
+    def render_fields(self, w, form, values):
+        w(u'<fieldset id="fs-%(divid)s">' % values)
+        fields = self._render_hidden_fields(w, form)
+        w(u'</fieldset>')
+        if fields:
+            self._render_fields(fields, w, form)
+        self.render_child_forms(w, form, values)
 
 
 class AutomaticEntityFormOrbui(AutomaticEntityForm):
@@ -221,13 +250,13 @@ class AutomaticEntityFormOrbui(AutomaticEntityForm):
     """
     cssclass = 'form-horizontal'
 
-
 # replace FormRenderer
 def registration_callback(vreg):
     """register new primary view for orbui project
     """
-    orbui_components = ((FormRendererOrbui, FormRenderer),
-                        (EntityFormRendererOrbui, EntityFormRenderer),
+    orbui_components = ((FormRendererOrbui, formrenderers.FormRenderer),
+                        (EntityFormRendererOrbui, formrenderers.EntityFormRenderer),
+                        (EntityInlinedFormRendererOrbui, formrenderers.EntityInlinedFormRenderer),
                         (AutomaticEntityFormOrbui, AutomaticEntityForm),
                         )
     vreg.register_all(globals().values(), __name__, [new for (new,old) in orbui_components])
