e51b08b2cb6e135d6596dcbf63599ce4e5f6b666
Ezequiel Bruni <ezequiel.bruni@gmail.com>
2012-12-10 16:49:04 -0600

    Commented out some code that was structurally unnecessary for the primary view. May cause trouble with other views.

--- views/themaintemplate.py
+++ views/themaintemplate.py
@@ -197,12 +197,12 @@ class TheMainTemplateOrbui(TheMainTemplate):
             columns = 9
         else:
             columns = 12
-        self.w(u'<section id="main">'
+        self.w(u'<!-- <section id="main">'
                u'<div class="container">'
-               u'<div class="row">')
+               u'<div class="row"> -->')
         # aside section - write boxes for this element
         self.nav_column(view, 'left')
-        self.w(u'<div class="span%i pull-right" id="pageContent">' % columns)
+        self.w(u'<!-- <div class="span%i pull-right" id="pageContent"> -->' % columns)
         if rqlcomp:
             rqlcomp.render(w=self.w, view=view)
         msgcomp = components.select_or_none('applmessages', self._cw,
@@ -224,10 +224,10 @@ class TheMainTemplateOrbui(TheMainTemplate):
         self.w(nav_html.getvalue())
         view.render(w=self.w)
         self.w(nav_html.getvalue())
-        self.w(u'</div>')
+        self.w(u'<!-- </div>')
         self.w(u'</div>'
                u'</div>'
-               u'</section>')
+               u'</section> -->')
 
     def page_footer(self, view):
         """display page footer
